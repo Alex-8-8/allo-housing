4ab26f50dcb201acbb6ea842884a610a3a4ff992
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-07-18 23:42:49 +0300

    test commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
-module.exports = {
-  "extends": "@mate-academy/eslint-config",
-  "env": {
-    "browser": true,
-    "jquery": true
-  }
-};
+// module.exports = {
+//   "extends": "@mate-academy/eslint-config",
+//   "env": {
+//     "browser": true,
+//     "jquery": true
+//   }
+// };
