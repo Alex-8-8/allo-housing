e66f90e8543e575f4bc85028cc2a0f813f0456e0
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-07-18 23:46:20 +0300

    test commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
-// module.exports = {
-//   "extends": "@mate-academy/eslint-config",
-//   "env": {
-//     "browser": true,
-//     "jquery": true
-//   }
-// };
+module.exports = {
+  "extends": "@mate-academy/eslint-config",
+  "parserOptions": {
+    "ecmaVersion": 2017
+  },
+  "env": {
+    "browser": true,
+    "jquery": true,
+    "es6": true
+  }
+};
